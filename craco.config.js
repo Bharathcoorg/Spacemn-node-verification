4185b5195d99683de8f0f72c1ea314242cbbbc93
Bharathcoorg <Bharathcoorg7@gmail.com>
2024-11-07 04:23:25 +0530

    Fix Deployment

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve = webpackConfig.resolve || {};
+      webpackConfig.resolve.fallback = {
+        assert: require.resolve('assert'),
+        buffer: require.resolve('buffer'),
+        crypto: require.resolve('crypto-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        path: require.resolve('path-browserify'),
+        process: require.resolve('process/browser'),
+        stream: require.resolve('stream-browserify'),
+        url: require.resolve('url'),
+      };
+      return webpackConfig;
+    },
+  },
+};
