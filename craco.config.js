ca424daa115107da3143f847b5eeb85dd2a6f32b
Bharathcoorg <Bharathcoorg7@gmail.com>
2024-11-07 05:03:12 +0530

    Fixes

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,28 @@
-const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      webpackConfig.resolve = webpackConfig.resolve || {};
       webpackConfig.resolve.fallback = {
         assert: require.resolve('assert'),
         buffer: require.resolve('buffer'),
         crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
         os: require.resolve('os-browserify/browser'),
         path: require.resolve('path-browserify'),
         process: require.resolve('process/browser'),
         stream: require.resolve('stream-browserify'),
         url: require.resolve('url'),
       };
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        })
+      );
+
       return webpackConfig;
     },
   },
