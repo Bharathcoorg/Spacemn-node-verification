b0698602cb0167e3788cd0a59b32c09091b4c146
Bharathcoorg <Bharathcoorg7@gmail.com>
2024-11-07 05:43:26 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import './polyfills';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Celo } from "@thirdweb-dev/chains";
+
+// Custom Celo Alfajores configuration
+const CeloAlfajores = {
+  ...Celo,
+  chainId: 44787, // Celo Alfajores chainId
+  name: "Celo Alfajores",
+  rpc: ["https://alfajores-forno.celo-testnet.org"],
+  nativeCurrency: {
+    name: "Celo",
+    symbol: "CELO",
+    decimals: 18,
+  },
+  testnet: true,
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain="celo-alfajores" clientId={process.env.REACT_APP_THIRDWEB_CLIENT_ID}>
+    <ThirdwebProvider activeChain={CeloAlfajores} clientId={process.env.REACT_APP_THIRDWEB_CLIENT_ID}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
